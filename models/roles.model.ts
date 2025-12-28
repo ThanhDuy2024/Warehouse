44eaa8fef8cb0c269f4a5eec154a9e56bc5f1602
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2025-12-28 10:23:19 +0700

    [Update] create table in model part2

---
+++ models/roles.model.ts
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../configs/database.config";
+
+export const Roles = sequelize.define("Roles", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    updatedBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+sequelize.sync({ alter: true });
