927dc49a35ef0aad7064c4679bae5f1cbea67e64
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2025-12-29 07:32:31 +0700

    [Update] Api get category

--- controllers/categories.controller.ts
+++ controllers/categories.controller.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { admin } from "../interfaces/admin.middlewares";
 import slugify from "slugify";
 import { Categories } from "../models/categories.model";
+import { Admin } from "../models/admin.model";
+import moment from "moment";
 
 export const createCategory = async (req: admin, res: Response) => {
     try {
@@ -12,7 +14,7 @@ export const createCategory = async (req: admin, res: Response) => {
         });
 
         await Categories.create(req.body);
-        
+
         res.json({
             code: "success",
             message: "Tao danh muc thanh cong"
@@ -22,6 +24,55 @@ export const createCategory = async (req: admin, res: Response) => {
         res.status(400).json({
             code: "error",
             message: "Tao danh muc loi"
+        })
+    }
+}
+
+export const getCategory = async (req: admin, res: Response) => {
+    try {
+        const categories = await Categories.findAll();
+
+        const data:Array<object> = []
+        for (const item of categories) {
+            const rawData = {
+                id: item.dataValues.id,
+                name: item.dataValues.name,
+                createdBy: "",
+                updatedBy: "",
+                createdAt: "",
+                updatedAt: ""
+            }
+
+            const createdAdmin = await Admin.findOne({
+                attributes: ["userName"],
+                where: {
+                    id: item.dataValues.createdBy
+                }
+            });
+
+            const updatedAdmin = await Admin.findOne({
+                attributes: ["userName"],
+                where: {
+                    id: item.dataValues.updatedBy
+                }
+            });
+
+            rawData.createdBy = createdAdmin?.dataValues.userName
+            rawData.updatedBy = updatedAdmin?.dataValues.userName
+
+            rawData.createdAt = moment(item.dataValues.createdAt).format("HH:mm - DD/MM/YYYY");
+            rawData.updatedAt = moment(item.dataValues.updatedAt).format("HH:mm - DD/MM/YYYY");
+
+            data.push(rawData);
+        }
+        res.json({
+            code: "success",
+            data: data
+        })
+    } catch (error) {
+        res.status(400).json({
+            code: "error",
+            message: "Loi lay data"
         })
     }
 }
--- routers/categories.route.ts
+++ routers/categories.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createCategory } from "../controllers/categories.controller";
+import { createCategory, getCategory } from "../controllers/categories.controller";
 
 const router = Router();
 
 router.post("/create", createCategory);
 
+router.get("/list", getCategory);
 export default router;
