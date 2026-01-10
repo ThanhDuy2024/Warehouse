4cd9af10b9a1f135c1f47c4fd49d2099d0a4ea0b
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2026-01-10 09:34:37 +0700

    [Update] set up cors

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import adminRouter from "./routers/index.route";
 import { connectDatabase } from "./configs/database.config";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +11,13 @@ const port = process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
 connectDatabase();
+
+app.use(cors({
+  origin: String(process.env.PORT_FE),
+  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.use("/api/admin", adminRouter);
 
